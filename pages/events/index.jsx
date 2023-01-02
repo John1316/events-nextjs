5450923e7cffbbb766d3109246b121a8d262dcfe
MEDO STORE <medostore@MEDOs-MacBook-Pro.local>
2023-01-02 17:51:52 +0200

    add search

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import EventList from "../../components/events/event-list";
+import EventsSearch from '../../components/events/event-search';
 import { getAllEvents } from '../../dummy-data';
 export default function AllEventsPage() {
     const events = getAllEvents();
 
   return (
-    <EventList items={events} />
+    <>
+        <EventsSearch />
+        <EventList items={events} />
+    </>
   )
 }
